fbec1c085b25751572925086c5a63d93e3be6533
zz-ederfs <161877224+zz-ederfs@users.noreply.github.com>
2024-08-01 18:09:16 +0000

    finished excuse generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let allSegments = [who, action, what, when];
+
+  function excuseGenerator() {
+    let newExcuse = [];
+    for (let index = 0; index < 4; index++) {
+      let singleSegment = allSegments[index];
+      let size = allSegments[index].length;
+      console.log(size);
+      let randomValue = Math.floor(Math.random() * (size - 1)) + 1; //  Math.floor(Math.random() * (max - min +1))+1 define los valores minimo y maximo incluyendo ambos
+      newExcuse.push(singleSegment[randomValue]);
+    }
+    document.getElementById("excuse").innerHTML = newExcuse.join(" ");
+  }
+  excuseGenerator();
 };
